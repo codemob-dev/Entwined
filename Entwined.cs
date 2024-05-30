7c2c61ecbdee40e5d57d8d1f114a5c229961d018
codemob <69110900+commandblox@users.noreply.github.com>
2024-05-30 14:55:23 -0400

    Packet registering system

--- Entwined.cs
+++ Entwined.cs
@@ -1,10 +1,11 @@
 ﻿using BepInEx;
-using BepInEx.Logging;
 using HarmonyLib;
 using Steamworks;
 using Steamworks.Data;
 using System;
+using System.Linq;
 using System.Runtime.InteropServices;
+using UnityEngine.SceneManagement;
 
 namespace Entwined
 {
@@ -16,8 +17,20 @@ namespace Entwined
     [BepInProcess("BoplBattle.exe")]
     public class Entwined : BaseUnityPlugin
     {
-        public Harmony harmony;
-        public static Entwined instance;
+        internal static Harmony harmony;
+        internal static Entwined instance;
+
+
+        /// <summary>
+        /// The signature to help avoid clashes with built-in packets. Can be any length.
+        /// </summary>
+        public static readonly byte[] signature = new byte[]
+        {
+            0b11101110,
+            0b10011011,
+            0b10010010,
+            0b01101100
+        };
 
         /// <summary>
         /// The main entrypoint. Performs all the harmony patches.
@@ -28,53 +41,65 @@ namespace Entwined
             harmony = new Harmony(Info.Metadata.GUID);
 
             harmony.PatchAll(); // Patch the entire assembly
+
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        }
+
+        
+        internal static bool loadedFirstScene = false;
+        /// <summary>
+        /// Runs on scene load. The first scene loads after all of the mods have loaded.
+        /// </summary>
+        private static void SceneManager_sceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (!loadedFirstScene)
+            {
+                loadedFirstScene = true;
+                IdentifierRegister.GeneratePacketIdentifierIDs();
+            }
         }
 
 
         /// <summary>
         /// <c>SteamSocket.OnMessage</c> handles all incoming messages. 
         /// If a custom packet is detected the patch method will prevent the 
-        /// original method from executing and handles that packet accordingly.
+        /// original method from executing and handle the packet accordingly.
         /// </summary>
         /// <returns><c>true</c> to allow the original method to execute and 
         /// <c>false</c> to prevent it from executing.</returns>
         [HarmonyPatch(typeof(SteamSocket), nameof(SteamSocket.OnMessage))]
         [HarmonyPrefix]
         private static bool SteamSocket_OnMessage(
-            SteamSocket __instance, Connection connection, 
-            NetIdentity identity, IntPtr data, int size, 
+            SteamSocket __instance, Connection connection,
+            NetIdentity identity, IntPtr data, int size,
             long messageNum, long recvTime, int channel)
         {
             // Copy the incoming packet to a buffer
             byte[] rawPacketBuffer = new byte[size];
             Marshal.Copy(data, rawPacketBuffer, 0, size);
 
-            // Check if the packet is custom
-            if (IsCustomPacket(rawPacketBuffer))
+            // Verify the packet is not custom
+            if (!IsCustomPacket(rawPacketBuffer)) return true;
+
+            // Check for invalid SteamIds
+            if (!identity.SteamId.IsValid)
             {
-                // Check for invalid SteamIds
-                if (!identity.SteamId.IsValid)
-                {
-                    instance.Logger.LogWarning("Received message from invalid SteamId!");
-                    return false;
-                }
-                // Create a Friend object and verify the connection is in the same lobby
-                Friend friend = new Friend(identity.SteamId);
-                if (!friend.IsIn(SteamManager.instance.currentLobby.Id))
-                {
-                    instance.Logger.LogWarning(
-                        $"{friend.Name} ({identity.SteamId}) sent a custom packet from outside of the current lobby!"
-                        );
-                    return false;
-                }
-
-                // TODO: make the actual advanced code part
-
+                instance.Logger.LogWarning("Received message from invalid SteamId!");
                 return false;
-            } else
-            {
-                return true;
             }
+            // Create a Friend object and verify the connection is in the same lobby
+            Friend friend = new Friend(identity.SteamId);
+            if (!friend.IsIn(SteamManager.instance.currentLobby.Id))
+            {
+                instance.Logger.LogWarning(
+                    $"{friend.Name} ({identity.SteamId}) sent a custom packet from outside of the current lobby!"
+                    );
+                return false;
+            }
+
+            // TODO: make the actual advanced code part
+
+            return false;
         }
 
 
@@ -83,9 +108,18 @@ namespace Entwined
         /// </summary>
         /// <param name="packet">The full packet data to verify</param>
         /// <returns><c>true</c> if the packet is a custom packet</returns>
-        private static bool IsCustomPacket(byte[] packet)
+        internal static bool IsCustomPacket(byte[] packet)
         {
-            return false;
+            if (packet.Length < signature.Length + PacketIdentifier.EncodedSize)
+            {
+                return false; // The packet is too small to be a custom packet
+            }
+            var packetSignature = packet.Take(signature.Length).ToArray();
+            if (packetSignature == signature)
+            {
+                return true; // The packet has the signature
+            }
+            return false; // The packet does not have the signature
         }
     }
 }
---
+++ IdentifierSystem.cs
@@ -0,0 +1,127 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+
+namespace Entwined
+{
+    /// <summary>
+    /// An identifier for a packet.
+    /// </summary>
+    internal class PacketIdentifier
+    {
+        public ushort PluginId { get; set; } = ushort.MaxValue; // Which plugin the packet is created in
+        public ushort PacketId { get; set; } = ushort.MaxValue; // The id of the packet
+
+        /// <summary>
+        /// Checks if the PacketIdentifier is ready for use
+        /// </summary>
+        public bool IsLoaded => PluginId != ushort.MaxValue || PacketId != ushort.MaxValue;
+
+
+        public static ushort EncodedSize => 4;
+
+        /// <summary>
+        /// Encode to a byte array
+        /// </summary>
+        /// <returns>The byte array</returns>
+        public byte[] Encode()
+        {
+
+            var PluginIdBytes = BitConverter.GetBytes(PluginId);
+            var PacketIdBytes = BitConverter.GetBytes(PacketId);
+
+            return new byte[] { PluginIdBytes[0], PluginIdBytes[1], 
+                                PacketIdBytes[0], PacketIdBytes[1] };
+        }
+
+        /// <summary>
+        /// Decode from a byte array
+        /// </summary>
+        /// <param name="bytes">The byte array</param>
+        /// <returns>The decoded <c>PacketIdentifier</c></returns>
+        public static PacketIdentifier Decode(byte[] bytes)
+        {
+            return new PacketIdentifier
+            {
+                PluginId = BitConverter.ToUInt16(bytes, 0),
+                PacketId = BitConverter.ToUInt16(bytes, 2)
+            };
+        }
+
+        /// <summary>
+        /// Check if the PacketIdentifier matches the data in the bytes
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public bool Matches(byte[] bytes)
+        {
+            return Encode() == bytes;
+        }
+    }
+
+
+    /// <summary>
+    /// Sorts the plugings by GUID to keep them consistent
+    /// </summary>
+    internal class PluginSorter : IComparer<BaseUnityPlugin>
+    {
+        public int Compare(BaseUnityPlugin x, BaseUnityPlugin y)
+        {
+            return x.Info.Metadata.GUID.CompareTo(y.Info.Metadata.GUID);
+        }
+    }
+
+    internal static class IdentifierRegister
+    {
+        internal static SortedList<BaseUnityPlugin, List<PacketIdentifier>> PluginPacketPairs = 
+            new SortedList<BaseUnityPlugin, List<PacketIdentifier>>(new PluginSorter());
+
+        internal static List<PacketIdentifier> packetIdentifiers = new List<PacketIdentifier>();
+
+        /// <summary>
+        /// Generates a packet identifier
+        /// </summary>
+        /// <param name="plugin">The plugin to generate the identifier under</param>
+        /// <returns>The packet identitier</returns>
+        internal static PacketIdentifier GenerateNewPacketIdentifier(BaseUnityPlugin plugin)
+        {
+            var identifier = new PacketIdentifier { };
+            if (PluginPacketPairs.ContainsKey(plugin))
+            {
+                PluginPacketPairs[plugin].Add(identifier);
+            } else
+            {
+                PluginPacketPairs.Add(plugin, new List<PacketIdentifier>());
+            }
+            return identifier;
+        }
+
+        internal static void GeneratePacketIdentifierIDs()
+        {
+            ushort pluginId = 0;
+            foreach (var pluginPackets in PluginPacketPairs)
+            {
+                var plugin = pluginPackets.Key;
+                var identifiers = pluginPackets.Value;
+
+                for (ushort packetId = 0; packetId < identifiers.Count; packetId++)
+                {
+                    var identifier = identifiers[packetId];
+                    identifier.PluginId = pluginId;
+                    identifier.PacketId = packetId;
+
+                    packetIdentifiers.Add(identifier);
+                }
+
+                pluginId++;
+            }
+        }
+
+        /// <summary>
+        /// Find the packet identifier matching the data.
+        /// </summary>
+        /// <param name="data">The data</param>
+        /// <returns>The decoded packet identifier</returns>
+        internal static PacketIdentifier GetPacketIdentifier(byte[] data) => packetIdentifiers.Find(id => id.Matches(data));
+    }
+}
